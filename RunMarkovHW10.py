09a8488e77c13547e8394eb05c23eed951ff0ee2
rtekolste <rebecca.tekolste@yale.edu>
2018-04-17 11:15:06 -0400

    V1

--- RunMarkovHW10.py
+++ RunMarkovHW10.py
@@ -1,9 +1,12 @@
-import ParameterClasses as P
-import MarkovClassesHW10 as MarkovCls
-import SupportMarkovHW10 as SupportMarkov
+#import ParameterClasses as P
+#import MarkovClassesHW10 as MarkovCls
+#import SupportMarkovHW10 as SupportMarkov
 import scr.SamplePathClasses as PathCls
 import scr.FigureSupport as Figs
 import InputDataHW10 as Data
+import Scratch as SupportMarkov
+import Scratch2 as P
+import Scratch3 as MarkovCls
 
 
 print("Problem 1")
@@ -14,14 +17,17 @@ cohort = MarkovCls.Cohort(
 
 simOutputs = cohort.simulate()
 
-cohort2 = MarkovCls.Cohort(
-    id=1,
-    therapy=P.Therapies.ANTICOAG)
+#cohort2 = MarkovCls.Cohort(
+#    id=1,
+#    therapy=P.Therapies.ANTICOAG)
 
-simOutputs2 = cohort2.simulate()
+#simOutputs2 = cohort2.simulate()
 
-SupportMarkov.print_outcomes(simOutputs, 'No treatment:')
-SupportMarkov.print_outcomes(simOutputs2, 'Anticoagulation treatment:')
+#SupportMarkov.print_outcomes(simOutputs, 'No treatment:')
+#SupportMarkov.print_outcomes(simOutputs2, 'Anticoagulation treatment:')
 
 print("Problem 2")
-SupportMarkov.print_comparative_outcomes(simOutputs, simOutputs2)
+#SupportMarkov.print_comparative_outcomes(simOutputs, simOutputs2)
+
+print("Problem 3")
+#SupportMarkov.report_CEA_CBA(simOutputs, simOutputs2)
